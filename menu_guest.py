cb0f5af8af24a0b397af1070f0205d415cf81529
deb <lellowgill@gmail.com>
2021-08-30 14:45:09 -0400

    add class restaurant

---
+++ menu_guest.py
@@ -0,0 +1,9 @@
+# Create a Restaurant class that handles all of the following dynamically
+class Restaurant:
+    def __init__(self, order_up = "ready", menu = "items", customer = "user_input" "total_cost = 0"):
+        self.customer = customer
+        self.menu = menu 
+        self.order_up = order_up
+        self.total_cost = total_cost
+        
+
