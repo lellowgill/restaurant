2bebd9b81229c713f1d7a1065bdea655be1fa19b
deb <lellowgill@gmail.com>
2021-08-30 15:26:36 -0400

    menu items added

--- menu_guest.py
+++ menu_guest.py
@@ -1,5 +1,5 @@
 # Create a Restaurant class that handles all of the following dynamically
-class Restaurant:
+class Restaurant():
     def __init__(self, order_up = "ready", menu = "items", customer = "user_input", total_cost = 0):
         self.customer = customer
         self.menu = menu 
@@ -9,17 +9,31 @@ class Restaurant:
 # consumer choice and name for order 
     def add_customer_name(self, name):
         self.customer["name"] = name 
-    def add_item(self,item):
-        self.items.append(item)
+    def add_items(self,items):
+        self.items.append(items)
     def get_total_cost(self):
         index = 0
-        total_cost = 0
-        
-    while index < len(self.items):
+        total_cost = 1
+
+    while index < len(self.menu):
         self.total_cost += self.items[index], ["total_cost"]
         index += 1
 
 first_order = order_up(True)
+
+first_order.add_consumer_name("Debbie")
+
+# dictionary created for the items on the list
+first_order.add_item({"Spinach", 10}, 
+{"Avocado", 8},
+{"Guava_juice, 6"}, 
+{"fresh_cabbage", 12}, 
+{"dressing", 2})
+first_order.sort()
+
+
+
+
 
 
 
